0a41f2d59e731a28cb4ad029501556259db33445
WolfEye <artemdobrovolksyi1@gmail.com>
2019-05-06 01:52:51 +0300

    changes complete

--- public/userInfo.php
+++ public/userInfo.php
@@ -73,7 +73,10 @@ session_start();
     </section>
 </section>
 <script src="scripts/script2.js"></script>
-<script src="scripts/jQuery.js"></script>
+<script
+        src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
 <script src="scripts/selectScript.js"></script>
 </body>
 </html>
